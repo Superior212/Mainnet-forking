d619fc46945f37617b12dc9c5f77cce2acf97425
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-09-17 13:42:05 +0100

    Refactor code to add SwapETHForExactTokens contract for token swapping

---
+++ test/SwapETHForExact.ts
@@ -0,0 +1,50 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import hre from "hardhat";
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+
+describe("TokenSwap", function () {
+  async function deployTokenSwap() {
+    const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    const [owner] = await hre.ethers.getSigners();
+
+    const TokenSwap = await hre.ethers.getContractFactory("SwapETHForExact");
+    const tokenSwap = await TokenSwap.deploy(ROUTER_ADDRESS);
+
+    return { tokenSwap, owner, ROUTER_ADDRESS };
+  }
+
+  describe("Swap", function () {
+    it("Should perform a token swap", async function () {
+      const { tokenSwap, ROUTER_ADDRESS } = await loadFixture(deployTokenSwap);
+      const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+      const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+      const TOKEN_HOLDER = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621";
+
+      await helpers.impersonateAccount(TOKEN_HOLDER);
+      const impersonatedSigner = await hre.ethers.getSigner(TOKEN_HOLDER);
+
+      const WETH_Contract = await hre.ethers.getContractAt(
+        "IERC20",
+        WETH,
+        impersonatedSigner
+      );
+      const amountIn = hre.ethers.parseEther("1"); // 1 WETH
+      await WETH_Contract.approve(tokenSwap, amountIn);
+
+      const path = [WETH, DAI];
+      const to = impersonatedSigner.address;
+      const deadline = Math.floor(Date.now() / 1000) + 60 * 10;
+
+      await tokenSwap.connect(impersonatedSigner).handleSwapETHForExact(
+        amountIn,
+        0, // amountOutMin, set to 0 for testing purposes
+        path,
+        to,
+        deadline
+      );
+
+      expect(await tokenSwap.swapCount()).to.equal(1);
+    });
+  });
+});
