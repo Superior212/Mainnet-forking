e96fac91979ffff0a5963c69e7f806f73eb44ae6
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-09-17 13:45:14 +0100

    Refactor code to rename deployTokenSwap to deploySwapETHForExact in SwapETHForExact.ts

--- test/SwapETHForExact.ts
+++ test/SwapETHForExact.ts
@@ -4,19 +4,19 @@ import hre from "hardhat";
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("TokenSwap", function () {
-  async function deployTokenSwap() {
+  async function deploySwapETHForExact() {
     const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
     const [owner] = await hre.ethers.getSigners();
 
-    const TokenSwap = await hre.ethers.getContractFactory("SwapETHForExact");
-    const tokenSwap = await TokenSwap.deploy(ROUTER_ADDRESS);
+    const SwapETHForExact = await hre.ethers.getContractFactory("SwapETHForExact");
+    const swapETHForExact = await SwapETHForExact.deploy(ROUTER_ADDRESS);
 
-    return { tokenSwap, owner, ROUTER_ADDRESS };
+    return { swapETHForExact, owner, ROUTER_ADDRESS };
   }
 
   describe("Swap", function () {
     it("Should perform a token swap", async function () {
-      const { tokenSwap, ROUTER_ADDRESS } = await loadFixture(deployTokenSwap);
+      const { swapETHForExact, ROUTER_ADDRESS } = await loadFixture(deploySwapETHForExact);
       const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
       const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
       const TOKEN_HOLDER = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621";
@@ -30,21 +30,21 @@ describe("TokenSwap", function () {
         impersonatedSigner
       );
       const amountIn = hre.ethers.parseEther("1"); // 1 WETH
-      await WETH_Contract.approve(tokenSwap, amountIn);
+      await WETH_Contract.approve(swapETHForExact, amountIn);
 
       const path = [WETH, DAI];
       const to = impersonatedSigner.address;
       const deadline = Math.floor(Date.now() / 1000) + 60 * 10;
 
-      await tokenSwap.connect(impersonatedSigner).handleSwapETHForExact(
+      await swapETHForExact.connect(impersonatedSigner).handleSwapETHForExact(
         amountIn,
-        0, // amountOutMin, set to 0 for testing purposes
+        0, // amountOutMin
         path,
         to,
         deadline
       );
 
-      expect(await tokenSwap.swapCount()).to.equal(1);
+      expect(await swapETHForExact.swapCount()).to.equal(1);
     });
   });
 });
